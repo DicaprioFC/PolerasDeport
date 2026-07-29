b0f069ccf02157eb3778b52a83d2f7afc3f10299
Leonardo <dicapriofc2004@gamil.com>
2026-07-29 19:31:25 -0400

    test: actualizar redireccion de autenticacion administrativa

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -30,7 +30,7 @@ class AuthenticationTest extends TestCase
 
         $response
             ->assertHasNoErrors()
-            ->assertRedirect(route('dashboard', absolute: false));
+            ->assertRedirect('/admin/dashboard');
 
         $this->assertAuthenticated();
     }
